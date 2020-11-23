b909327ce2d5ec2cfc70f79627d7c4aa057fa6e3
Alvyn Abranches <alvynabranches@gmail.com>
2020-11-23 23:33:28 +0530

    23112020v2

---
+++ fe/php.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+cd %1
+mv index.html index.php
+mv contact-us.html contact-us.php
+mv login.html login.php
+mv profile.html profile.php
+mv register.html register.php
+mv user.html user.php
+mv user-contact-us.html user-contact-us.php
