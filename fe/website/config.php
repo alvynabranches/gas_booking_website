3ba102b25a3929871020073156991c98bb9216fa
Alvyn Abranches <alvynabranches@gmail.com>
2020-11-24 22:07:56 +0530

    24112020v1

---
+++ fe/website/config.php
@@ -0,0 +1,10 @@
+<?php
+    $host="localhost";
+    $username="root";
+    $password="";
+    $db="project";
+    $con=new mysqli($host,$username,$password,$db);
+    if($con->connect_error){die("Connection failed: ".$con->connect_error);}
+    function console_log($m){echo "<script>console.log('$m')</script>";}
+    $sql="select * from location;";$result=mysqli_query($con,$sql);while($row=mysqli_fetch_assoc($result)){echo "<option value='".$row['location_id']."'>".$row['location_name']."</option>";}
+?>
