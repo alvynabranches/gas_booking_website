5ed91d36fa70efea3c4b1152877e102c6180f9a9
Alvyn Abranches <alvynabranches@gmail.com>
2020-11-26 00:13:59 +0530

    26112020v4

--- website/config.php
+++ website/config.php
@@ -11,21 +11,12 @@
     function console_warn($m){echo "<script>console.warn('$m')</script>";}
     function console_error($m){echo "<script>console.error('$m')</script>";}
     function console_info($m){echo "<script>console.info('$m')</script>";}
+    function redirect(string $redirect_page, bool $is_log_in=FALSE){if(!isset($_COOKIE['PHPSESSID'])){session_start();}if($_SESSION==null){if(!$is_log_in){header("Location: $redirect_page");}}else{if($is_log_in){header("Location: $redirect_page");}}}
     function location_options($sql="select * from location order by location_name;",$host='localhost',$username='root',$password='',$db='gas_booking_system'){$con=new mysqli($host,$username,$password,$db);$result=mysqli_query($con,$sql);$options="";while($row=mysqli_fetch_assoc($result)){$options.="<option value='".$row['location_id']."'>".$row['location_name']."</option>";};return $options;}
-    function redirect(string $redirect_page, bool $is_log_in=FALSE){
-        if(!isset($_COOKIE['PHPSESSID'])){
-            session_start();
-        }
-        if($_SESSION==null){
-            if(!$is_log_in){
-                header("Location: $redirect_page");
-            }
-        }
-        else{
-            if($is_log_in){
-                header("Location: $redirect_page");
-            }
-        }
+    function exec_query($sql,$host='localhost',$username='root',$password='',$db='gas_booking_system'){$con=new mysqli($host,$username,$password,$db);return mysqli_query($con,$sql);}
+    function login($username, $password){
+        if(!isset($_COOKIE['PHPSESSID'])){session_start();}
+        
     }
     function logout(){session_start();session_destroy();}
     function register(){
