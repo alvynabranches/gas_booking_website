c05be0a27314627f2802952587905490795fe09e
Alvyn Abranches <alvynabranches@gmail.com>
2020-11-26 22:57:35 +0530

    26112020v22

--- website/script.js
+++ website/script.js
@@ -61,6 +61,24 @@ document.getElementById('username').addEventListener('keyup', (e) => {
         document.getElementById('username').style.borderWidth = '5px';
     }
 });
+document.getElementById('username').addEventListener('focus', (e) => {
+    if(document.getElementById('username').value.length < 8) {
+        document.getElementById('username').style.borderColor = 'red';
+        document.getElementById('username').style.borderWidth = '5px';
+    }else{
+        document.getElementById('username').style.borderColor = 'green';
+        document.getElementById('username').style.borderWidth = '5px';
+    }
+});
+document.getElementById('username').addEventListener('focusout', (e) => {
+    if(document.getElementById('username').value.length < 8) {
+        document.getElementById('username').style.borderColor = 'red';
+        document.getElementById('username').style.borderWidth = '5px';
+    }else{
+        document.getElementById('username').style.borderColor = 'green';
+        document.getElementById('username').style.borderWidth = '5px';
+    }
+});
 // document.getElementById('email').addEventListener('keyup', (e) => {
 //     let reg = /^([A-Za-z0-9_\-\.])+@([A-Za-z0-9_\-\.])+\a([A-Za-z]{2,4})$/;
 //     if(reg.test(document.getElementById('email'))){
