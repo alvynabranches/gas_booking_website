aa6ab82a4d210022b46b597a107c3c88bacb3200
Alvyn Abranches <alvynabranches@gmail.com>
2020-11-27 13:38:16 +0530

    27112020v17

--- website/config.php
+++ website/config.php
@@ -1,12 +1,15 @@
 <?php
-    $host="localhost";$username="root";$password="";$db="gas_booking_system";$con=new mysqli($host,$username,$password,$db);
     error_reporting(0);session_start();
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('PASSWORD', '');
+    define('DB', 'gas_booking_system');
     define('REPORT', TRUE);
+    $con=new mysqli(DB_SERVER,DB_USERNAME,PASSWORD,DB);
     function console_log($m){echo "<script>console.log('$m')</script>";}
     function console_warn($m){echo "<script>console.warn('$m')</script>";}
     function console_error($m){echo "<script>console.error('$m')</script>";}
     function console_info($m){echo "<script>console.info('$m')</script>";}
-    if($con->connect_error){console_error("Connection failed to mysql");die("Connection failed: ".$con->connect_error);}
     function redirect(string $redirect_page, bool $is_log_in=FALSE){if(!isset($_COOKIE['PHPSESSID'])){session_start();}if($_SESSION==null){if(!$is_log_in){header("Location: $redirect_page");}}else{if($is_log_in){header("Location: $redirect_page");}}}
     function exec_query($sql,$host='localhost',$username='root',$password='',$db='gas_booking_system'){$con=new mysqli($host,$username,$password,$db);$result=mysqli_query($con,$sql);$con->close();return $result;}
     function location_options($sql="select * from location order by location_name;"){$result=exec_query($sql);$options="";if($result->num_rows>0){while($row=mysqli_fetch_assoc($result)){$options.="<option value='".$row['location_id']."'>".$row['location_name']."</option>";}};return $options;}
@@ -14,5 +17,6 @@
     function logout(){session_start();session_unset();if(REPORT){console_info('Logged Out Successfully!');}}
     function logout_user(){if(isset($_GET['action'])){if($_GET['action'] == 'logout'){logout();}}}
     function register(){if(isset($_POST['full_name'])&&isset($_POST['full_address'])&&isset($_POST['username'])&&isset($_POST['email'])&&isset($_POST['password'])&&isset($_POST['confirm_password'])&&isset($_POST['location'])&&isset($_POST['type'])&&isset($_POST['phone_no'])){$name=$_POST['full_name'];$address=$_POST['full_address'];$user=$_POST['username'];$email=$_POST['email'];$pwd=$_POST['password'];$cnf_pwd=$_POST['confirm_password'];$location=$_POST['location'];$type=$_POST['type'];$phone=$_POST['phone_no'];$pwd=password_hash($pwd, PASSWORD_DEFAULT);$sql="insert into customer(customer_name, customer_address, username, customer_email, password, customer_location_id, customer_type, customer_no) values ('$name', '$address', '$user', '$email', '$pwd', '$location', '$type', '$phone')";if(exec_query($sql)===TRUE){if(REPORT){console_info("Record Created Successfully");}echo "<script>let baseurl=window.location.origin;let loginurl=baseurl+'/project/website/login.php';setTimeout(function(){window.location.href=loginurl;},0);</script>";}else{if(REPORT){console_warn("Record Not Created Successfully");}}}}
+    if($con->connect_error){console_error("Connection failed to mysql");die("Connection failed: ".$con->connect_error);}
     // if($pwd == $cnf_pwd){$pwd=password_hash($pwd);}
 ?>
