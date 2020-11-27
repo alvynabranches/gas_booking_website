411d5973d4b12179447bf3c98063b39cf0169b4d
Alvyn Abranches <alvynabranches@gmail.com>
2020-11-27 13:12:07 +0530

    27112020v15

--- website/user-contact-us.php
+++ website/user-contact-us.php
@@ -24,7 +24,7 @@
                     <li class="nav-item" role="presentation"><a class="nav-link active" data-bs-hover-animate="flash" style="color: #ffffff;font-weight: bold;" href="contact-us.php">Contact Us</a></li>
                     <li class="nav-item dropdown"><a class="dropdown-toggle nav-link btn-primary" data-toggle="dropdown" aria-expanded="false" data-bs-hover-animate="wobble" href="" style="color: #ffffff;font-weight: bold;" name="username-container"><?php if(isset($_SESSION['username'])){echo $_SESSION['username'];} ?></a>
                         <div
-                            class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="">Profile</a><a class="dropdown-item" role="presentation" href="user-contact-us.php?action=logout">Logout<?php include_once("config.php"); logout_user(); redirect('index.php', FALSE); ?></a></div>
+                            class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="user.php">Profile</a><a class="dropdown-item" role="presentation" href="user-contact-us.php?action=logout">Logout<?php include_once("config.php"); logout_user(); redirect('index.php', FALSE); ?></a></div>
         </li>
         </ul>
         </div>
@@ -43,5 +43,4 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/script.min.js"></script>
 </body>
-
 </html>
