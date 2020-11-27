a4c7747748d5c05a7e3064e19e0d09f363e02825
Alvyn Abranches <alvynabranches@gmail.com>
2020-11-27 11:14:06 +0530

    27112020v4

--- website/user.php
+++ website/user.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="assets/css/styles.min.css">
     <?php include_once("config.php"); redirect('index.php', FALSE); ?>
+    
 </head>
 
 <body>
@@ -24,7 +25,7 @@
                     <li class="nav-item" role="presentation"><a class="nav-link" data-bs-hover-animate="flash" style="color: #ffffff;font-weight: bold;" href="user-contact-us.php">Contact Us</a></li>
                     <li class="nav-item dropdown"><a class="dropdown-toggle nav-link text-capitalize btn-primary" data-toggle="dropdown" aria-expanded="false" data-bs-hover-animate="wobble" href="" style="color: #ffffff;font-weight: bold;" name="username-container">User</a>
                         <div class="dropdown-menu"
-                            role="menu"><a class="dropdown-item" role="presentation" href="">Profile</a><a class="dropdown-item" role="presentation" href="#">Logout</a></div>
+                            role="menu"><a class="dropdown-item" role="presentation" href="">Profile</a><a class="dropdown-item" role="presentation" href="user.php?action=logout">Logout<?php include_once("config.php"); logout_user(); redirect('index.php', FALSE); ?></a></div>
                     </li>
                 </ul>
         </div>
