89b04a16ef97f4117f1a46c06cd6a4e53b1922bc
Alvyn Abranches <alvynabranches@gmail.com>
2020-11-27 12:45:21 +0530

    27112020v12

--- website/user-contact-us.php
+++ website/user-contact-us.php
@@ -22,9 +22,9 @@
                 class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item" role="presentation"><a class="nav-link active" data-bs-hover-animate="flash" style="color: #ffffff;font-weight: bold;" href="contact-us.php">Contact Us</a></li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link text-capitalize btn-primary" data-toggle="dropdown" aria-expanded="false" data-bs-hover-animate="wobble" href="" style="color: #ffffff;font-weight: bold;" name="username-container">Username</a>
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link btn-primary" data-toggle="dropdown" aria-expanded="false" data-bs-hover-animate="wobble" href="" style="color: #ffffff;font-weight: bold;" name="username-container"><?php if(isset($_SESSION['username'])){echo $_SESSION['username'];} ?></a>
                         <div
-                            class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="">Profile</a><a class="dropdown-item" role="presentation" href="#">Logout</a></div>
+                            class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="">Profile</a><a class="dropdown-item" role="presentation" href="user-contact-us.php?action=logout">Logout<?php include_once("config.php"); logout_user(); redirect('index.php', FALSE); ?></a></div>
         </li>
         </ul>
         </div>
