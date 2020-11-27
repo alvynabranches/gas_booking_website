50bbe43acd7fd7ac0bc5f8dc4e6752794766ed61
Alvyn Abranches <alvynabranches@gmail.com>
2020-11-27 19:10:55 +0530

    27112020v31

--- fe/php.bat
+++ fe/php.bat
@@ -7,4 +7,5 @@ mv profile.html profile.php
 mv register.html register.php
 mv user.html user.php
 mv user-booking.html user-booking.php
+mv user-change-password.html user-change-password.php
 mv user-contact-us.html user-contact-us.php
