4774f4e4a9a3a68768fb2a81bab789de272c06b6
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-02 00:01:58 +0530

    02112020v5

--- website/script.js
+++ website/script.js
@@ -50,8 +50,7 @@ document.getElementById('full_address').addEventListener('focus', (e) => {
 });
 document.getElementById('full_address').addEventListener('focusout', (e) => {
     if (document.getElementById('full_address').value.length >= 16){
-        document.getElementById('full_address').style.borderColor = 'green';
-        document.getElementById('full_address').style.borderWidth = '5px';
+        document.getElementById('password').style.border = '5px solid green';
     }else{
         document.getElementById('password').style.border = '5px solid red';
     }
