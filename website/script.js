202797ee0b8845a20ee8aff163d0b42b69c82608
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-03 00:41:45 +0530

    03112020v7

--- website/script.js
+++ website/script.js
@@ -19,65 +19,65 @@ document.getElementById('password').addEventListener('keyup', (e) => {
 });
 document.getElementById('full_name').addEventListener('keydown', (e) => {
     if(document.getElementById('full_name').value.length >= 5){
-        document.getElementById('password').style.border = '5px solid green';
+        document.getElementById('full_name').style.border = '5px solid green';
     }else{
-        document.getElementById('password').style.border = '5px solid red';
+        document.getElementById('full_name').style.border = '5px solid red';
     }
 });
 document.getElementById('full_name').addEventListener('focus', (e) => {
     if(document.getElementById('full_name').value.length >= 5){
-        document.getElementById('password').style.border = '5px solid green';
+        document.getElementById('full_name').style.border = '5px solid green';
     }else{
-        document.getElementById('password').style.border = '5px solid red';
+        document.getElementById('full_name').style.border = '5px solid red';
     }
 });
 document.getElementById('full_name').addEventListener('focusout', (e) => {
     if(document.getElementById('full_name').value.length >= 5){
-        document.getElementById('password').style.border = '5px solid green';
+        document.getElementById('full_name').style.border = '5px solid green';
     }else{
-        document.getElementById('password').style.border = '5px solid red';
+        document.getElementById('full_name').style.border = '5px solid red';
     }
 });
 document.getElementById('full_address').addEventListener('keyup', (e) => {
     if (document.getElementById('full_address').value.length >= 16){
-        document.getElementById('password').style.border = '5px solid green';
+        document.getElementById('full_address').style.border = '5px solid green';
     }else{
-        document.getElementById('password').style.border = '5px solid red';
+        document.getElementById('full_address').style.border = '5px solid red';
     }
 });
 document.getElementById('full_address').addEventListener('focus', (e) => {
     if (document.getElementById('full_address').value.length >= 16){
-        document.getElementById('password').style.border = '5px solid green';
+        document.getElementById('full_address').style.border = '5px solid green';
     }else{
-        document.getElementById('password').style.border = '5px solid red';
+        document.getElementById('full_address').style.border = '5px solid red';
     }
 });
 document.getElementById('full_address').addEventListener('focusout', (e) => {
     if (document.getElementById('full_address').value.length >= 16){
-        document.getElementById('password').style.border = '5px solid green';
+        document.getElementById('full_address').style.border = '5px solid green';
     }else{
-        document.getElementById('password').style.border = '5px solid red';
+        document.getElementById('full_address').style.border = '5px solid red';
     }
 });
 document.getElementById('username').addEventListener('keyup', (e) => {
     if(document.getElementById('username').value.length < 8) {
-        document.getElementById('password').style.border = '5px solid red';
+        document.getElementById('username').style.border = '5px solid red';
     }else{
-        document.getElementById('password').style.border = '5px solid green';
+        document.getElementById('username').style.border = '5px solid green';
     }
 });
 document.getElementById('username').addEventListener('focus', (e) => {
     if(document.getElementById('username').value.length < 8) {
-        document.getElementById('password').style.border = '5px solid red';
+        document.getElementById('username').style.border = '5px solid red';
     }else{
-        document.getElementById('password').style.border = '5px solid green';
+        document.getElementById('username').style.border = '5px solid green';
     }
 });
 document.getElementById('username').addEventListener('focusout', (e) => {
     if(document.getElementById('username').value.length < 8) {
-        document.getElementById('password').style.border = '5px solid red';
+        document.getElementById('username').style.border = '5px solid red';
     }else{
-        document.getElementById('password').style.border = '5px solid green';
+        document.getElementById('username').style.border = '5px solid green';
     }
 });
 // document.getElementById('email').addEventListener('keyup', (e) => {
