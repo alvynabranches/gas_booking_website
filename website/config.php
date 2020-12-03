c51274d01209cc05f318dc6d9b39b88dc1e6a66d
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-03 22:07:12 +0530

    03112020v180

--- website/config.php
+++ website/config.php
@@ -48,17 +48,29 @@
                         alert("Successfully Updated Customer Name, Customer Phone No. and Customer Address!");
                     }
                 }elseif(($new_name!=$name)&&($new_phone!=$phone)&&($new_address==$address)){
-                    $sql="UPDATE customer SET customer_name='$new_name' customer_no='$new_phone' WHERE customer_id='$id';";
+                    if(exec_query("UPDATE customer SET customer_name='$new_name' customer_no='$new_phone' WHERE customer_id='$id';")){
+
+                    }
                 }elseif(($new_name!=$name)&&($new_phone==$phone)&&($new_address!=$address)){
-                    $sql="UPDATE customer SET customer_name='$new_name' customer_address='$new_address' WHERE customer_id='$id';";
+                    if(exec_query("UPDATE customer SET customer_name='$new_name' customer_address='$new_address' WHERE customer_id='$id';")){
+
+                    }
                 }elseif(($new_name==$name)&&($new_phone!=$phone)&&($new_address!=$address)){
-                    $sql="UPDATE customer SET customer_no='$new_phone' customer_address='$new_address' WHERE customer_id='$id';";
+                    if(exec_query("UPDATE customer SET customer_no='$new_phone' customer_address='$new_address' WHERE customer_id='$id';")){
+
+                    }
                 }elseif(($new_name!=$name)&&($new_phone==$phone)&&($new_address==$address)){
-                    $sql="UPDATE customer SET customer_name='$new_name' WHERE customer_id='$id';";
+                    if(exec_query("UPDATE customer SET customer_name='$new_name' WHERE customer_id='$id';")){
+
+                    }
                 }elseif(($new_name==$name)&&($new_phone!=$phone)&&($new_address==$address)){
-                    $sql="UPDATE customer SET customer_no='$new_phone' WHERE customer_id='$id';";
+                    if(exec_query("UPDATE customer SET customer_no='$new_phone' WHERE customer_id='$id';")){
+
+                    }
                 }elseif(($new_name==$name)&&($new_phone==$phone)&&($new_address!=$address)){
-                    $sql="UPDATE customer SET customer_address='$new_address' WHERE customer_id='$id';";
+                    if(exec_query("UPDATE customer SET customer_address='$new_address' WHERE customer_id='$id';")){
+
+                    }
                 }else{
                     if(REPORT){console_log("Everything is up to date!");}
                     alert("Everything is up to date!");
