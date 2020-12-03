6a22283c7c129037e7733085cd61467a05c8e0c9
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-04 00:50:31 +0530

    04112020v76

--- website/config.php
+++ website/config.php
@@ -29,7 +29,7 @@
         if($result->num_rows>0){
             while($row=mysqli_fetch_assoc($result)){
                 $record=$row['bd'];
-                $records.="<tr><td>$record</td></tr>";
+                $records.="<tr><td>".$record."</td></tr>";
             }
         }else{$records="No Records Found";}
         return $records;
