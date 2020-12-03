f4f2eabcd37f6225e2b68ec828999bc363de143e
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-03 23:33:38 +0530

    03112020v290

--- website/config.php
+++ website/config.php
@@ -43,8 +43,7 @@
                     }else{if(REPORT){console_log("Unsuccessfully Updated Customer Name and Customer Phone No.!");}alert("Unsuccessfully Updated Customer Name and Customer Phone No.!");
                     }}elseif(($new_name!=$name)&&($new_phone==$phone)&&($new_address!=$address)){
                     if(exec_query("UPDATE customer SET customer_name='$new_name', customer_address='$new_address' WHERE customer_id='$id';")){$_SESSION['name']=$new_name;$_SESSION['address']=$new_address;if(REPORT){console_log("Successfully Updated Customer Name and Customer Address!");}alert("Successfully Updated Customer Name and Customer Address!");
-                    }else{
-                        if(REPORT){console_log("Unsuccessfully Updated Customer Name and Customer Address!");}alert("Unsuccessfully Updated Customer Name and Customer Address!");}
+                    }else{if(REPORT){console_log("Unsuccessfully Updated Customer Name and Customer Address!");}alert("Unsuccessfully Updated Customer Name and Customer Address!");}
                 }elseif(($new_name==$name)&&($new_phone!=$phone)&&($new_address!=$address)){
                     if(exec_query("UPDATE customer SET customer_no='$new_phone' customer_address='$new_address' WHERE customer_id='$id';")){
                         $_SESSION['phone_no']=$new_phone;$_SESSION['address']=$new_address;if(REPORT){console_log("Successfully Updated Customer Phone No. and Customer Address!");}alert("Successfully Updated Customer Phone No. and Customer Address!");
