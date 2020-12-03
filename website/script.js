667faa94557141363e55bca2923a52d9c3349387
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-03 12:15:53 +0530

    03112020v32

--- website/script.js
+++ website/script.js
@@ -79,8 +79,7 @@ document.getElementById('username').addEventListener('focus', (e) => {
     if(document.getElementById('username').value.length < 8) {
         document.getElementById('username').style.border = '5px solid red';
     }else{
-        document.getElementById('username').style.borderColor = 'green';
-        document.getElementById('username').style.borderWidth = '5px';
+        document.getElementById('username').style.border = '5px solid green';
     }
 });
 document.getElementById('username').addEventListener('focusout', (e) => {
