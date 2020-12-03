a6f905e0153fc8dc861e6c29b98b76fad9528154
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-04 00:46:09 +0530

    04112020v66

--- website/config.php
+++ website/config.php
@@ -50,7 +50,7 @@
     // echo pending_orders(9);
     // echo "<br><br><br>";
     // echo delivered_orders(9);
-    echo "<br><br><br>";
+    // echo "<br><br><br>";
 ?>
 <table><?php echo pending_orders(9);?></table>
 <table><?php echo delivered_orders(9);?></table>
