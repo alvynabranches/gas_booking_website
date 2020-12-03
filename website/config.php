24de8e9150fff7b4aca776329ff690fd0d23f73d
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-03 23:26:04 +0530

    03112020v244

--- website/config.php
+++ website/config.php
@@ -100,8 +100,7 @@
                         $_SESSION['address']=$new_address;if(REPORT){console_log("Successfully Updated Customer Address!");}alert("Successfully Updated Customer Address!");
                     }else{if(REPORT){console_log("Unsuccessfully Updated Customer Address!");}alert("Unsuccessfully Updated Customer Address!");}
                 }else{
-                    if(REPORT){console_log("Everything is up to date!");}alert("Everything is up to date!");
-                    redirect("user.php",TRUE);}}else{alert("Password does not match!");console_log("Password does not match!");redirect('user-settings.php',TRUE);}}}if($con->connect_error){console_error("Connection failed to mysql");die("Connection failed: ".$con->connect_error);}
+                    if(REPORT){console_log("Everything is up to date!");}alert("Everything is up to date!");redirect("user.php",TRUE);}}else{alert("Password does not match!");console_log("Password does not match!");redirect('user-settings.php',TRUE);}}}if($con->connect_error){console_error("Connection failed to mysql");die("Connection failed: ".$con->connect_error);}
     // console_log(date('Y-m-d H:i:s'));
     // print_r($_SESSION);
     // session_unset();
