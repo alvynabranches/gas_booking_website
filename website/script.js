e5c575a2532c1549285a9339e17566604b085283
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-03 12:15:22 +0530

    03112020v30

--- website/script.js
+++ website/script.js
@@ -97,7 +97,6 @@ document.getElementById('username').addEventListener('focusout', (e) => {
 //         document.getElementById('email').style.borderColor = 'green';
 //         document.getElementById('email').style.borderWidth = '5px';
 //     }else{
-//         document.getElementById('email').style.borderColor = 'red';
-//         document.getElementById('email').style.borderWidth = '5px';
+//         document.getElementById('email').style.border = '5px solid red';
 //     }
 // });
