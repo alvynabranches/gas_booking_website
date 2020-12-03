fc44dcadec8a35decf8c391c67fc047637043d00
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-03 23:44:34 +0530

    03112020v324

--- website/config.php
+++ website/config.php
@@ -38,8 +38,7 @@
         }
     }
     function pending_orders($id){
-        return exec_query("SELECT * FROM booking WHERE id='$id' AND booking_status='pending';");
-    }
+        return exec_query("SELECT * FROM booking WHERE id='$id' AND booking_status='pending';");}
     function delivered_orders($id){return exec_query("SELECT * FROM booking WHERE id='$id' AND booking_status='delivered';");}
     // console_log(date('Y-m-d H:i:s'));
     // print_r($_SESSION);
