f55c1d53041bc191e11c18ad7d49f19ba3100e92
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-03 23:53:12 +0530

    03112020v349

--- website/config.php
+++ website/config.php
@@ -27,5 +27,5 @@
     function delivered_orders($id){return exec_query("SELECT * FROM booking WHERE id='$id' AND booking_status='delivered';");}
     
     // console_log(date('Y-m-d H:i:s'));
-    // print_r($_SESSION);
+    
 ?>
