9b46586581fcdfb293a320d5cdd80dc94efa0637
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-04 00:30:40 +0530

    04112020v43

--- website/config.php
+++ website/config.php
@@ -38,7 +38,7 @@
         $result=exec_query("SELECT booking_date AS bd FROM booking WHERE id='$id' AND booking_status='delivered';");
         if($result->num_rows>0){
 
-        }else{}
+        }else{$records="No Records Found";}
         return $records;
     }
 ?>
