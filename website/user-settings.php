17addf82be00bb9c9db240cf92e809146168f853
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-03 16:22:00 +0530

    03112020v111

--- website/user-settings.php
+++ website/user-settings.php
@@ -23,7 +23,7 @@
                     <li class="nav-item" role="presentation"><a class="nav-link" data-bs-hover-animate="flash" style="color: #ffffff;font-weight: bold;" href="contact-us.php">Contact Us</a></li>
                     <li class="nav-item dropdown"><a class="dropdown-toggle nav-link btn-primary" data-toggle="dropdown" aria-expanded="false" data-bs-hover-animate="wobble" href="" style="color: #ffffff;font-weight: bold;" name="username-container">username</a>
                         <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="user.php">Profile</a><a class="dropdown-item" role="presentation" href="user-booking.php">Booking</a><a class="dropdown-item" role="presentation" href="user-order-status.php">Order Status</a>
-                            <a class="dropdown-item" role="presentation" href="user-change-password.php">Change Password</a><a class="dropdown-item active" role="presentation" href="user-settings.php">Settings</a><a class="dropdown-item" role="presentation" href="user-settings.php?action=logout">Logout<?php include_once("config.php"); logout_user();redirect('index.php',FALSE);?></a></div>
+                            <a class="dropdown-item" role="presentation" href="user-change-password.php">Change Password</a><a class="dropdown-item active" role="presentation" href="user-settings.php">Settings</a><a class="dropdown-item" role="presentation" href="user-settings.php?action=logout">Logout<?php include_once("config.php");logout_user();redirect('index.php',FALSE);?></a></div>
                     </li>
                 </ul>
         </div>
