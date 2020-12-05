108bcc208a773ea7e9738e06b2a5a02bce7314b8
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-05 14:00:38 +0530

    05112020v37

--- website/index.php
+++ website/index.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="assets/css/styles.min.css">
-    <?php include_once("config.php"); redirect('user.php',TRUE);?>
     <?php include_once("config.php");redirect('user-contact-us.php','agency-contact-us.php',TRUE,'contact-us.php');?>
 </head>
 
