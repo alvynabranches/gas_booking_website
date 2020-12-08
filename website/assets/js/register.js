eaa6221fd064166359a48a8acc7883ef0c2bc49a
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-08 13:30:14 +0530

    08122020v22

--- website/assets/js/register.js
+++ website/assets/js/register.js
@@ -63,7 +63,7 @@ document.getElementById('username').addEventListener('keyup',(e)=>{
     if(document.getElementById('username').value.length<8) {
         document.getElementById('username').style.border='5px solid red';
     }else{
-        document.getElementById('username').style.border= '5px solid green';
+        document.getElementById('username').style.border='5px solid green';
     }
 });
 document.getElementById('username').addEventListener('focus', (e) => {
