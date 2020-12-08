85afdae6e6728461efe857a4408f75f23d28c70d
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-08 13:41:31 +0530

    08122020v68

--- website/assets/js/register.js
+++ website/assets/js/register.js
@@ -101,7 +101,7 @@ document.getElementById('phone_no').addEventListener('focusout',(e)=>{
         document.getElementById('phone_no').style.border='5px solid green';
     }
 });
-// document.getElementById('email').addEventListener('keyup',(e) => {
+// document.getElementById('email').addEventListener('keyup',(e) =>{
 //     let reg = /^([A-Za-z0-9_\-\.])+@([A-Za-z0-9_\-\.])+\a([A-Za-z]{2,4})$/;
 //     if(reg.test(document.getElementById('email'))){
 //         document.getElementById('email').style.border='5px solid green';
