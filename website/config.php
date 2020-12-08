ba211204aa6d02964cceb894eda536ca7f46b1e8
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-08 15:50:45 +0530

    08122020v91

--- website/config.php
+++ website/config.php
@@ -34,14 +34,13 @@
                         $_SESSION['name']=$new_name;
                         $_SESSION['phone_no']=$new_phone;
                         $_SESSION['address']=$new_address;
-                        if(DEBUG){
-                            console_log("Successfully Updated Customer Name, Customer Phone No. and Customer Address!");
-                        }
+                        if(DEBUG){console_log("Successfully Updated Customer Name, Customer Phone No. and Customer Address!");}
                         alert("Successfully Updated Customer Name, Customer Phone No. and Customer Address!");
                     }else{
                         if(DEBUG){console_log("Unsuccessfully Updated Customer Name, Customer Phone No. and Customer Address!");
                     }alert("Unsuccessfully Updated Customer Name, Customer Phone No. and Customer Address!");
-            }}elseif(($new_name!=$name)&&($new_phone!=$phone)&&($new_address==$address)){
+                }
+            }elseif(($new_name!=$name)&&($new_phone!=$phone)&&($new_address==$address)){
                 if(exec_query("UPDATE customer SET customer_name='$new_name', customer_no='$new_phone' WHERE customer_id='$id';")){
                     $_SESSION['name']=$new_name;$_SESSION['phone_no']=$new_phone;
                     if(DEBUG){
