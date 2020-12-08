9fb9afbdb557aca8fcd8e4ac491a6bd82bede429
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-08 13:34:00 +0530

    08122020v53

--- website/assets/js/register.js
+++ website/assets/js/register.js
@@ -89,7 +89,7 @@ document.getElementById('phone_no').addEventListener('keyup',(e)=>{
 });
 document.getElementById('phone_no').addEventListener('focus',(e)=>{
     if(document.getElementById('phone_no').value.length!=10){
-        document.getElementById('phone_no').style.border= '5px solid red';
+        document.getElementById('phone_no').style.border='5px solid red';
     }else{
         document.getElementById('phone_no').style.border = '5px solid green';
     }
