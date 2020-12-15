42d86160f1f2082ac162ffe04ab210371aa9672f
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-15 11:37:51 +0530

    14122020v2

--- website/user-booking.php
+++ website/user-booking.php
@@ -32,7 +32,7 @@
     <div class="scroller">
         <div class="form-div container-fluid h-100">
             <form class="login-form" method="POST">
-                <div class="form-group d-flex"><label class="d-flex justify-content-xl-start align-items-xl-center" style="font-weight: bold;font-size: 3vh;padding-top: 7px;">Amount&nbsp;&nbsp;</label><input class="form-control-plaintext d-flex read-only-text-input" type="text" id="amount"value="<?php if($_SESSION['type']=='domestic'){echo 608;}else{echo 1200;}?>" readonly="" name="amount" placeholder="amount"></div>
+                <div class="form-group d-flex"><label class="d-flex justify-content-xl-start align-items-xl-center" style="font-weight: bold;font-size: 3vh;padding-top: 7px;">Amount&nbsp;&nbsp;</label><input class="form-control-plaintext d-flex read-only-text-input" type="text" id="amount"value="<?php if($_SESSION['type']=='domestic'){echo 658;}else{echo 1200;}?>" readonly="" name="amount" placeholder="amount"></div>
                 <div class="form-group"><select class="form-control" id="payment_option" name="payment_option"><option value="" selected="">Payment Option</option><option value="cod">Cash On Delivery</option><option value="prepaid">Prepaid</option></select></div>
                 <div class="form-group"><select class="form-control" id="book_now" name="book_now"><option value="" selected="">Book Now?</option><option value="0">No</option><option value="1">Yes</option></select></div>
                 <div class="btn-group d-flex pt-3" role="group"><button class="btn btn-success btn-rounded book-btn" data-bs-hover-animate="bounce" type="submit">Book Now</button></div>
