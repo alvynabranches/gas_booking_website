47520e631f03aec45e216a556636c33e101394e1
Alvyn Abranches <alvynabranches@gmail.com>
2020-12-17 22:15:23 +0530

    17122020v29

--- website/config.php
+++ website/config.php
@@ -35,6 +35,8 @@
     }
 
     function undelivered($b_id){
-        if(exec_query("UPDATE booking SET booking_status='pending' WHERE booking_id=$b_id")){}
+        if(exec_query("UPDATE booking SET booking_status='pending' WHERE booking_id=$b_id")){
+            
+        }
     }
 ?>
